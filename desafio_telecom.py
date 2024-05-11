7fdf67d82613f352fc19edf7f3a9ec3aed92bf33
Rick Farias <167890284+Rickfarias@users.noreply.github.com>
2024-05-11 17:56:24 -0300

    feat: update

--- desafio_telecom.py
+++ desafio_telecom.py
@@ -4,15 +4,12 @@ def recomendar_plano(consumo):
         if consumo <= 10:
             print("Plano Essencial Fibra - 50Mbps.")
         
-        elif consumo > 10 or consumo <= 20:
+        elif consumo > 10 or consumo < 20:
             print("Plano Prata Fibra - 100Mbps.")
         
-        elif consumo >= 20:
-            print("Plano Premium Fibra - 300Mbps.")
-
         else:
-            print("Operação falhou! Valor informado é inválido, tente novamente.")
-
+            print("Plano Premium Fibra - 300Mbps.")
+            
 consumo = float(input())
 plano_recomendado = recomendar_plano(consumo=consumo)
-print(f"{recomendar_plano}")
+print(recomendar_plano(consumo))
